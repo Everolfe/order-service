a12f91c459fa6522661d7c55ecb9e3715a3bfe40
Everolfe <rusakgeorgy@gmail.com>
2026-04-03 10:52:10 +0300

    250403-3: fix ci connection to db

--- src/test/java/com/github/everolfe/orderservice/integration/config/TestSecurityConfig.java
+++ src/test/java/com/github/everolfe/orderservice/integration/config/TestSecurityConfig.java
@@ -2,32 +2,33 @@ package com.github.everolfe.orderservice.integration.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.mockito.Mockito.mock;
 
 @TestConfiguration
 public class TestSecurityConfig {
 
     @Bean
-    @Primary
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/items/**").hasRole("ADMIN")
-                        .requestMatchers( HttpMethod.GET, "/api/items/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
-                .httpBasic(httpBasic -> {});
+                .csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return mock(JwtDecoder.class);
     }
 
     @Bean
